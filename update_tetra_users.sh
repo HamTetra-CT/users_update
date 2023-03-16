69ffe9d98aa508578a66ba858615676fe4429884
Francisco (F4VSE) <kikosgc@users.noreply.github.com>
2023-03-16 18:07:01 +0100

    Better user experience
    Some modifications for better user experience with some output.

--- update_tetra_users.sh
+++ update_tetra_users.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+# Script by HamTetra-CT Dev Team
+echo "A inicializar..."
+sleep 1
 
 # Download the latest version of the file from GitHub
+echo "A iniciar transferência do novo ficheiro tetra_users..."
 wget -q https://raw.githubusercontent.com/HamTetra-CT/users_update/main/tetra_users.json -O /tmp/tetra_users.json
 
 # Compare the contents of the two files
+echo "A comparar versões..."
 if ! cmp -s /tmp/tetra_users.json /etc/svxlink/tetra_users.json; then
    # Replace the local file with the updated version
+   echo "Nova versão instalada!"
    sudo cp /tmp/tetra_users.json /etc/svxlink/tetra_users.json
 
    # Delete the temporary file
    rm /tmp/tetra_users.json
 fi
+echo "Atualização terminada!"
+sleep 1
